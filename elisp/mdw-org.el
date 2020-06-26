e62e2c97a4202e300a4598b1a21aad5ad23778ad
humanfactors <michaelwilson1991@gmail.com>
2020-06-26 12:22:26 +0800

    This looks ok

--- elisp/mdw-org.el
+++ elisp/mdw-org.el
@@ -29,12 +29,11 @@
 	     (list "ti" "#+TITLE: ?\n"))
 
 (custom-set-faces
-  '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
-  '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
-  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+  '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-)
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 
 (defun org-wrap-source ()
   (interactive)
