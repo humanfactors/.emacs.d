e10a6f2fd389c8bfc9c9a998fd30ebc5d05bfab4
Michael David Wilson <michaelwilson1991@gmail.com>
2020-08-21 00:13:34 +0800

    xah

--- elisp/mdw-utilities.el
+++ elisp/mdw-utilities.el
@@ -2,8 +2,6 @@
 ;;
 ;;; Code:
 
-
-
 ;; Utility Functionality
 (defun michael-timestamp ()
   "Insert a timestamp at the current point.
@@ -77,14 +75,12 @@ Uses `bjk-timestamp-format' for formatting the date/time."
   `(when (eq system-type ',type)
      ,@body))
 
-
 ;; Function for finding out info about font at cursor
 (defun what-face (pos)
   (interactive "d")
   (let ((face (or (get-char-property (point) 'read-face-name)
 		  (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
-
 
 (defun dwim-backward-kill-word ()
   "DWIM kill characters backward until encountering the beginning of a
@@ -107,7 +103,6 @@ word or non-word."
   (while (looking-at "[^a-zA-Z0-9\s\n]")
     (backward-char))
   (forward-char))
-
 
 (defun aborn/backward-kill-word ()
   "Customize/Smart backward-kill-word."
@@ -282,6 +277,81 @@ A prefix arg for filling means justify (as for `fill-paragraph')."
 (global-set-key (kbd "<f6>") 'dot-emacs)
 
 ;; end fommil/dotfiles/
+
+;; ergomacs
+
+(defun xah-delete-blank-lines ()
+  "Delete all newline around cursor.
+
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02"
+  (interactive)
+  (let ($p3 $p4)
+	  (skip-chars-backward "\n")
+	  (setq $p3 (point))
+	  (skip-chars-forward "\n")
+	  (setq $p4 (point))
+	  (delete-region $p3 $p4)))
+
+(defun xah-shrink-whitespaces ()
+  "Remove whitespaces around cursor to just one, or none.
+
+Shrink any neighboring space tab newline characters to 1 or none.
+If cursor neighbor has space/tab, toggle between 1 or 0 space.
+If cursor neighbor are newline, shrink them to just 1.
+If already has just 1 whitespace, delete it.
+
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02T14:38:04-07:00"
+  (interactive)
+  (let* (
+	 ($eol-count 0)
+	 ($p0 (point))
+	 $p1 ; whitespace begin
+	 $p2 ; whitespace end
+	 ($charBefore (char-before))
+	 ($charAfter (char-after ))
+	 ($space-neighbor-p (or (eq $charBefore 32) (eq $charBefore 9) (eq $charAfter 32) (eq $charAfter 9)))
+	 $just-1-space-p
+	 )
+    (skip-chars-backward " \n\t")
+    (setq $p1 (point))
+    (goto-char $p0)
+    (skip-chars-forward " \n\t")
+    (setq $p2 (point))
+    (goto-char $p1)
+    (while (search-forward "\n" $p2 t )
+      (setq $eol-count (1+ $eol-count)))
+    (setq $just-1-space-p (eq (- $p2 $p1) 1))
+    (goto-char $p0)
+    (cond
+     ((eq $eol-count 0)
+      (if $just-1-space-p
+	  (delete-horizontal-space)
+	(progn (delete-horizontal-space)
+	       (insert " "))))
+     ((eq $eol-count 1)
+      (if $space-neighbor-p
+	  (delete-horizontal-space)
+	(progn (xah-delete-blank-lines) (insert " "))))
+     ((eq $eol-count 2)
+      (if $space-neighbor-p
+	  (delete-horizontal-space)
+	(progn
+	  (xah-delete-blank-lines)
+	  (insert "\n"))))
+     ((> $eol-count 2)
+      (if $space-neighbor-p
+	  (delete-horizontal-space)
+	(progn
+	  (goto-char $p2)
+	  (search-backward "\n" )
+	  (delete-region $p1 (point))
+	  (insert "\n"))))
+     (t (progn
+	  (message "nothing done. logic error 40873. shouldn't reach here" ))))))
+
+(global-set-key (kbd "C-c C-\\") 'xah-shrink-whitespaces)
 
 (provide 'mdw-utilities)
 ;; End of mdw-utilities.el
