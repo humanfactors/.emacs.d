a0eb453ec5b7296d82e076a7be71fae7171256ad
humanfactors <16096044+humanfactors@users.noreply.github.com>
2020-10-15 15:19:10 +0800

    Bloody UTF8

--- elisp/mdw-defaults.el
+++ elisp/mdw-defaults.el
@@ -3,9 +3,11 @@
 ;;; Code:
 
 ;; UTF-8
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
 
 ;; Editing config
 (setq mouse-yank-at-point nil) ;; Fixes bug associated with middle click paste for spell check
